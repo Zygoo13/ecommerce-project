82040a788de47580ba959bf8e2652e4b64290602
Zygoo13 <AnhHQ.B23CN022@stu.ptit.edu.vn>
2025-09-10 01:28:18 +0700

    Initial commit - upload source project

---
+++ ecommerce-web-parent/ecommerce-backend/src/main/java/com/zygoo13/admin/EcommerceBackendApplication.java
@@ -0,0 +1,12 @@
+package com.zygoo13.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
+}
---
+++ ecommerce-web-parent/ecommerce-frontend/src/main/java/com/zygoo13/site/EcommerceFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.zygoo13.site;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceFrontendApplication.class, args);
+    }
+}
